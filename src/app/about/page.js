000a79f6c52d75cbec26e6fdfdc098c4a0ddb4b1
Daniisalas875308 <875308@unizar.es>
2025-02-26 12:21:03 +0100

    Primer commit del proyecto Next.js

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+      <main>
+        <h1>Sobre Nosotros</h1>
+        <p>Bienvenido a la página de Sobre Nosotros.</p>
+      </main>
+    );
+  }
---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+    return (
+      <main>
+        <h1>Contacto</h1>
+        <p>Puedes contactarnos en contacto@miweb.com.</p>
+      </main>
+    );
+  }
+  
